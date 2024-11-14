0ecb5dadeaaf47082623da844be9f7cd3bf01aef
luis50019 <teddiazdiaz019@gmail.com>
2024-11-13 19:51:46 -0600

    Ejercicio 5 terminado

--- Ejercicios_Orientados/src/Ejercicio_5/JuegoDelAhorcado.java
+++ Ejercicios_Orientados/src/Ejercicio_5/JuegoDelAhorcado.java
@@ -4,6 +4,12 @@
  */
 package Ejercicio_5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Alumnos
@@ -12,35 +18,94 @@ public class JuegoDelAhorcado {
     private String palabra;
     private int numeroDeAciertos;
     private String palabraOculta;
+    private int numeroErrores;
+    private int numeroIntentos = 7;
+    private boolean juegoTerminado;
+    private List<String> palabras;
     
     private void ocultarPalabra(String palabra){
         this.palabraOculta = palabra.replaceAll("[a-zA-Z]", "_ ");
     }
+
+    public void setPalabra(String palabra) {
+        this.palabra = palabra;
+    }
+
+    public boolean isJuegoTerminado() {
+        return juegoTerminado;
+    }
             
-    public JuegoDelAhorcado(String palabra) {
+    public JuegoDelAhorcado() {
         this.palabra = palabra;
         this.numeroDeAciertos = 0;
+        this.numeroErrores = 0;
+        this.juegoTerminado = true;
+        this.palabras = new ArrayList<>(Arrays.asList("hola","celular","teclado","computadora"));
+    }
+    public void inicarJuego(){
+        this.numeroDeAciertos = 0;
+        this.numeroErrores = 0;
+        this.juegoTerminado = false;
+        
+        Random random = new Random();
+        int randomItem = random.nextInt(palabras.size());
+        setPalabra(palabras.get(randomItem));
         ocultarPalabra(palabra);
         
     }
+    public void finalizarJuego(){
+        if(this.numeroDeAciertos == palabra.length()){
+            System.out.println("\nHas ganado !!\n");
+            this.juegoTerminado = true;
+        }
+        if(this.numeroErrores == this.numeroIntentos){
+            System.out.println("\nHas perdido\n");
+            this.juegoTerminado = true;
+        }
+        
+    }
     
-    public void verificarLetra(int posicion, char letraIngresada){
-        char letraAValidar = palabra.charAt(posicion);
+    public void validarCaracter(char caracter){
+        String regex = "^[a-zA-Z]+$";
+        String cadena = Character.toString(caracter);
+        if(!Pattern.matches(regex, cadena)){
+            throw new IllegalArgumentException("La letra no puede ser un numero y simbolos especiales");
+        }
+        return;
+    }
+    
+    public boolean verificarLetra(int posicion, char letraIngresada){
+        if(posicion > this.palabra.length()){
+            throw new ArrayIndexOutOfBoundsException("La posicion excede la longitud de la palabra");
+        }
+        char letraAValidar = this.palabra.charAt(posicion-1);
+        
+        
         
         if(letraAValidar == letraIngresada){
             this.numeroDeAciertos += 1;
             StringBuilder temp = new StringBuilder(palabraOculta);
-            temp.setCharAt(posicion+1, letraAValidar);
+            if(posicion == 1){
+                temp.setCharAt(posicion-1, letraAValidar);
+            }else{
+                temp.setCharAt((posicion-1)*2, letraAValidar);
+            }
+            System.out.println("correcto!!");
             this.palabraOculta = temp.toString();
-            return ;
-        }        
-        System.out.println(letraAValidar);
+            return true;
+        }
+        
+        this.numeroErrores += 1;
+        System.out.println("letra incorecta!!");
+        return false;
     }
     
-    public void mostrarPalabra(){
-        System.out.println("Palabra:");
+    public void mostrarInfo(){
+        System.out.println("\nPalabra:");
         System.out.println(palabraOculta);
+        System.out.println("Letras acertadas: "+numeroDeAciertos);
+        System.out.println("Letras restantes: "+ (palabra.length()-this.numeroDeAciertos) );
+        System.out.println("Intentos restantes: "+(this.numeroIntentos-this.numeroErrores));
     }
     
-    //- - 
 }
--- Ejercicios_Orientados/src/Ejercicio_5/Main.java
+++ Ejercicios_Orientados/src/Ejercicio_5/Main.java
@@ -4,22 +4,65 @@
  */
 package Ejercicio_5;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author Alumnos
  */
 public class Main {
     public static void main(String[] args){
-       JuegoDelAhorcado juego = new JuegoDelAhorcado("hola");
+        Scanner input = new Scanner(System.in);
+        JuegoDelAhorcado juego = new JuegoDelAhorcado();
+        boolean juegoTerminado = false;
+        int opcion = 0;
+        char letra;
        
-       juego.mostrarPalabra();
-       juego.verificarLetra(0,'h');
-       juego.mostrarPalabra();
-       juego.verificarLetra(1,'o');
-       juego.mostrarPalabra();
-       juego.verificarLetra(2,'l');
-       juego.mostrarPalabra();
-       juego.verificarLetra(3,'a');
-       juego.mostrarPalabra();
+       
+        do {            
+            if(juego.isJuegoTerminado()){
+                System.out.println("----- MENU -----");
+                System.out.println("1 ----- iniciar juego");
+                System.out.println("2 ----- salir");
+                System.out.print("Opcion: ");
+                try {
+                    opcion = input.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("\nopcion no valida\n");
+                    input.nextLine();
+                    continue;
+                }
+            }
+            if(opcion == 1){
+                juego.inicarJuego();
+                opcion = 0;
+            }
+            if(opcion == 2){
+                System.out.println("\nFin del juego...\n");
+                break;
+            }
+            juego.mostrarInfo();
+            try {
+                System.out.print("\nPosicion: ");
+                int posicion = input.nextInt();
+                System.out.print("Letra: ");
+                letra = input.next().charAt(0);
+                juego.validarCaracter(letra);
+                juego.verificarLetra(posicion, letra);
+                juego.finalizarJuego();
+                
+            } catch(InputMismatchException e){
+                System.out.println("Error: La posicion debe de ser un numero");
+                input.nextLine();
+            }catch (IllegalArgumentException e) {
+                System.out.println("Error: "+e.getMessage());
+            }catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Error: "+e.getMessage());
+            }
+
+        } while (opcion != 2);
+       
+
     }
 }
